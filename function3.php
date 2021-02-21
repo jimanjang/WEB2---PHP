a08fa5da24e3479296647dadf7299e61f50a1c48
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-02-22 05:42:12 +0900

    make function

---
+++ function3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    function basic(){
+        print("Lorem ipsum dolor1<br>");
+        print("Lorem ipsum dolor2<br>");
+    }
+    basic();
+    basic();
+    basic();
+</body>
+</html>
