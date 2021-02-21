00250b19bba66d804840d343140ddc907aa2ccb4
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-02-22 05:43:39 +0900

    Add files via upload

---
+++ function4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    function sum($left, $right){
+        print($left+$right);
+        print("<br>");
+    }
+    sum(2,4);
+    sum(4,6);
+</body>
+</html>
